d6271241d1136152a32dc50d8013c72154dc635b
sixmik1313 <venigermathis@icloud.com>
2026-02-15 23:57:51 +0100

    Add files via upload

---
+++ beast-mode-tracker/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'beast-mode-v1';
+const urlsToCache = [
+  './fitness-tracker.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
